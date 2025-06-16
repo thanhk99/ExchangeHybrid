3db362d509d51ee06ce83175133f928a08761b2f
chithanh <chipngaocho123@gmail.com>
2025-06-16 23:59:38 +0700

    security v1

--- src/app/security/security.component.ts
+++ src/app/security/security.component.ts
@@ -9,8 +9,26 @@ import { FormsModule } from '@angular/forms';
   styleUrls: ['./security.component.css']
 })
 export class Security {
+  // Trạng thái modal
+  showPhoneModal = false;
+  showEmailModal = false;
+
+  // Dữ liệu form
+  newPhone = '';
+  newEmail = '';
+  verificationCode = '';
+  remainingAttempts = 3;
+  selectedCountryCode = '+84';
+  newPhoneCode = '';
+  emailCode = '';
+  currentPhoneCode = '';
+  newEmailCode = '';
+  currentEmailCode = '';
+  phoneCode = '';
+
   user = {
     email: 'chipngaocho123@gmail.com',
+    phone: '0971823008',
   };
 
   securityItems = [
@@ -48,5 +66,82 @@ export class Security {
 
   maskPhone(phone: string): string {
     return '*******' + phone.slice(-3);
+  }
+
+  // Mở modal
+  openPhoneModal() {
+    this.showPhoneModal = true;
+    this.newPhone = '';
+    this.verificationCode = '';
+  }
+
+  openEmailModal() {
+    this.showEmailModal = true;
+    this.newEmail = '';
+    this.verificationCode = '';
+  }
+
+  // Đóng modal
+  closeModal() {
+    this.showPhoneModal = false;
+    this.showEmailModal = false;
+  }
+
+  // Gửi mã xác thực
+  sendVerificationCode() {
+    // Logic gửi mã ở đây
+    console.log('Mã xác thực đã được gửi');
+  }
+
+  // Xác nhận thay đổi
+  confirmChange() {
+    if (this.showPhoneModal) {
+      console.log('Đổi số điện thoại thành công:', this.newPhone);
+    } else if (this.showEmailModal) {
+      console.log('Đổi email thành công:', this.newEmail);
+    }
+    this.closeModal();
+  }
+
+  sendPhoneCode() {
+    console.log('Gửi mã xác thực SMS đến số mới');
+    // Logic gửi mã
+  }
+
+  sendEmailCode() {
+    console.log('Gửi mã xác thực qua email');
+    // Logic gửi mã
+  }
+
+  sendCurrentPhoneCode() {
+    console.log('Gửi mã xác thực SMS đến số hiện tại');
+    // Logic gửi mã
+  }
+
+  sendNewEmailCode() {
+    console.log('Gửi mã xác thực đến email mới');
+    // Logic gửi mã
+  }
+
+  sendCurrentEmailCode() {
+    console.log('Gửi mã xác thực đến email hiện tại');
+    // Logic gửi mã
+  }
+
+  sendPhoneVerificationCode() {
+    console.log('Gửi mã xác thực SMS');
+    // Logic gửi mã
+  }
+
+  confirmPhoneChange() {
+    console.log('Xác nhận thay đổi số điện thoại');
+    // Logic xác nhận
+    this.closeModal();
+  }
+
+  confirmEmailChange() {
+    console.log('Xác nhận thay đổi email');
+    // Logic xác nhận
+    this.closeModal();
   }
 }
