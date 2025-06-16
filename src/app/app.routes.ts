aa5f4d9fb63691ab1f8d588456c4e92c14a7e1cc
ndt2-0-1-2-4 <nguyentri1o2.z@gmail.com>
2025-06-16 14:39:32 +0700

    homepagev1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { HomepageComponent } from './homepage/homepage.component';
+export const routes: Routes = [
+    {path: 'homepage', component: HomepageComponent},
+];
