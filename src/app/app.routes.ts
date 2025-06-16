ed3da42b3f9e44e29c87dcf0025630b48e65f60c
chithanh <chipngaocho123@gmail.com>
2025-06-17 00:20:49 +0700

    Profile and Security

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { ProfileComponent } from './profile/profile.component';
 import { Security } from './security/security.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 export const routes: Routes = [
-  { path: 'profile', component: ProfileComponent },
-  { path: 'security', component: Security }
+    { path: 'profile', component: ProfileComponent },
+    { path: 'security', component: Security },
+    { path: '', component: HomepageComponent },
+    { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
+
