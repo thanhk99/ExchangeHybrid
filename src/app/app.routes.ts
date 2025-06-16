66f06dbb27b5bcbed2660534f0f85b0b0dbf063e
chithanh <chipngaocho123@gmail.com>
2025-06-16 22:18:25 +0700

    Profile v1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'profile', component: ProfileComponent }
+];
