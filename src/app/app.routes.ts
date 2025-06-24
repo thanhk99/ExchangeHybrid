7a923c19619f2610ec42368727d7a6c6ba2c90b7
PT-Giang <truonggiang10052k4@gmail.com>
2025-06-24 21:03:52 +0700

    register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,14 @@ import { ProfileComponent } from './profile/profile.component';
 import { Security } from './security/security.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { DeviceManagement } from './device-management/device-management';
+import { Component } from '@angular/core';
+import { LoginRegister } from './login-register/login-register';
 
 export const routes: Routes = [
-    { path: '', component: HomepageComponent },
-    { path: 'profile', component: ProfileComponent },
-    { path: 'security', component: Security },
-    { path: 'device-management', component: DeviceManagement },
-    { path: '**', redirectTo: '', pathMatch: 'full' },
-
+  { path: '', component: HomepageComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'security', component: Security },
+  { path: 'device-management', component: DeviceManagement },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
+  { path: 'login-register', component: LoginRegister },
 ];
-
