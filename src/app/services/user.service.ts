2fe9af1c07c7da6418a533be43f564646dc245fa
ndt2-0-1-2-4 <nguyentri1o2.z@gmail.com>
2025-08-01 17:17:19 +0700

    add logout mobilescreen

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,12 +1,45 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, of, tap } from 'rxjs';
+import { environment } from '../../environments/environment.development';
+
+export interface User {
+  id: number;
+  name: string;
+  email: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  private user: any = null;
+  constructor(
+    private http : HttpClient
+  ) { }
+  private cachedUser: User | null = null;
+  private user$ = new BehaviorSubject<User | null>(null);
 
-  constructor() { }
+  fetchUser(): Observable<User> {
+    if (this.cachedUser) {
+      return of(this.cachedUser); 
+    }
+
+    return this.http.get<User>(environment.apiGetUser).pipe(
+      tap(user => {
+        this.cachedUser = user;
+        this.user$.next(user);
+      })
+    );
+  }
+
+  getUserStream(): Observable<User | null> {
+    return this.user$.asObservable();
+  }
+
+  clearCache() {
+    this.cachedUser = null;
+    this.user$.next(null);
+  }
 
 
 }
