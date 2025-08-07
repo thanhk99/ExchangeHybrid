453c04f748260db74a3826ec5f143f5d7f0ebffb
ndt2-0-1-2-4 <nguyentri1o2.z@gmail.com>
2025-08-07 22:59:40 +0700

    merge ndt

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,17 @@ import { Security } from './security/security.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { DeviceManagement } from './device-management/device-management';
 import { Assetpage } from './assetpage/assetpage';
-import { FundingWallet } from './funding-wallet/funding-wallet';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { Buy } from './buy/buy';
+import { P2pmarket } from './p2pmarket/p2pmarket';
+import { Methodpayment } from './methodpayment/methodpayment';
+import { SelectAdvertisement } from './select-advertisement/select-advertisement';
+import { CreateAds } from './create-ads/create-ads';
+import { Nextstep } from './create-ads/nextstep/nextstep';
+import { MyTransaction } from './my-transaction/my-transaction';
+import { MyProfile } from './my-profile/my-profile';
+import { BigTransaction } from './big-transaction/big-transaction';
 
 export const routes: Routes = [
     { path: '', component: HomepageComponent },
@@ -12,6 +22,16 @@ export const routes: Routes = [
     { path: 'security', component: Security },
     { path: 'device-management', component: DeviceManagement },
     { path: 'assetpage', component: Assetpage },
-    { path: 'funding-wallet', component: FundingWallet },
+    { path: 'register', component: RegisterComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'buy', component: Buy },
+    { path: 'p2pmarket', component: P2pmarket },
+    { path: 'methodpayment', component: Methodpayment },
+    { path: 'select-advertisement', component: SelectAdvertisement },
+    { path: 'create-ads', component: CreateAds },
+    { path: 'create-ads/nextstep', component: Nextstep },
+    { path: 'my-transaction', component: MyTransaction },
+    { path: 'my-profile', component: MyProfile },
+    { path: 'big-transaction', component: BigTransaction },
     { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
